f56379cc11f015d6a8ada997e64f3ca6fb0bbc5a
himanshu1100 <its.hu1910@gmail.com>
2026-04-26 10:03:28 +0530

    .

--- server/config/db.js
+++ server/config/db.js
@@ -1,24 +1,11 @@
-// const mongoose = require('mongoose');
-
-// const connectDB = async () => {
-//   try {
-//     const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/smart-job-portal');
-//     console.log(`MongoDB Connected: ${conn.connection.host}`);
-//   } catch (error) {
-//     console.error(`Error: ${error.message}`);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB;
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB Connected");
+    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/smart-job-portal');
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.log(error.message);
+    console.error(`Error: ${error.message}`);
     process.exit(1);
   }
 };
